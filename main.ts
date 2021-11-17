a54ab51756433949e2ccc5217269f178d8257fc0
Johan Gorter <johan-gorter@users.noreply.github.com>
2021-11-17 11:41:42 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -76,12 +76,30 @@ basic.forever(function () {
     }
     if (scene == 2) {
         basic.showLeds(`
-            . # . . .
             . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
             `)
+    }
+    while (scene == 3) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            # # # # #
+            # . # . #
+            # # # # #
+            `)
+        basic.pause(50)
+        basic.showLeds(`
+            . . . . .
+            # . # . #
+            # # # # #
+            . # . # .
+            # # # # #
+            `)
+        basic.pause(50)
     }
     basic.pause(100)
 })
@@ -120,15 +138,20 @@ basic.forever(function () {
             music.ringTone(185)
             basic.pause(50)
         }
-        if (scene == 1) {
-            for (let index = 0; index <= 5; index++) {
-                music.setVolume(100 - 20 * index)
-                music.ringTone(147)
-                basic.pause(50)
-                music.ringTone(185)
-                basic.pause(50)
-            }
+    }
+    if (scene == 1) {
+        for (let index = 0; index <= 5; index++) {
+            music.setVolume(100 - 20 * index)
+            music.ringTone(147)
+            basic.pause(50)
+            music.ringTone(185)
+            basic.pause(50)
         }
+    }
+    if (scene == 3) {
+        music.setVolume(100)
+        music.ringTone(randint(100, 400))
+        basic.pause(20)
     }
     music.stopAllSounds()
     basic.pause(100)
